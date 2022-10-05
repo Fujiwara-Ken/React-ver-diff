fe4b75c5105c76f0279f23b264139cff730ce522
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-10-05 21:39:26 +0900

    strict console.log add

--- react17/src/App.tsx
+++ react17/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("Appがレンダリング react17だと1回表示");
   return (
     <div className="App">
       <header className="App-header">
--- react18-changed/src/App.tsx
+++ react18-changed/src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("Appがレンダリング react18だと2回表示される");
+
   return (
     <div className="App">
       <header className="App-header">
