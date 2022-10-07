89c1febe277128f742e5a4eb8e6a08904e8d922f
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-10-07 14:42:51 +0900

    transition add

--- react18-changed/src/App.tsx
+++ react18-changed/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { AutoBatchEventHandler } from "./components/AutoBatchEventHandler";
 import { AutoBatchOther } from "./components/AutoBatchOther";
+import { Transition } from "./components/Transition";
 
 function App() {
   console.log("Appがレンダリング react18だと2回表示される");
@@ -10,6 +11,8 @@ function App() {
     <div className="App">
       <AutoBatchEventHandler />
       <AutoBatchOther />
+      <hr />
+      <Transition />
     </div>
   );
 }
