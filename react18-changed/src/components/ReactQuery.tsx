d0f0443110e24a53c99e6458754d65584289db1c
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-10-09 13:51:58 +0900

    typo fix

--- react18-changed/src/components/ReactQuery.tsx
+++ react18-changed/src/components/ReactQuery.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from "react";
+import { Suspense, useState, useTransition } from "react";
 import { Sidebar } from "./Sidebar";
 import { TodoList } from "./TodoList";
 import { ErrorBoundary } from "react-error-boundary";
@@ -8,6 +8,24 @@ type Tabs = "todo" | "album";
 
 export const ReactQuery = () => {
   const [selectedTab, setSelectedTab] = useState<Tabs>("todo");
+  const [isPending, startTransition] = useTransition();
+
+  const buttonStyle = {
+    padding: "12px",
+    fontSize: "16px",
+    border: "none",
+    // opacity: isPending ? 0.5 : 1,
+  };
+  const albumButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: selectedTab === "album" ? "royalblue" : "white",
+    color: selectedTab === "album" ? "white" : "black",
+  };
+  const todoButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: selectedTab === "todo" ? "royalblue" : "white",
+    color: selectedTab === "todo" ? "white" : "black",
+  };
 
   const onClickTabButton = (tab: Tabs) => {
     setSelectedTab(tab);
@@ -17,8 +35,18 @@ export const ReactQuery = () => {
     <div style={{ display: "flex", padding: "16px" }}>
       <Sidebar />
       <div style={{ flexGrow: 1 }}>
-        <button onClick={() => onClickTabButton("todo")}>Todo</button>
-        <button onClick={() => onClickTabButton("album")}>Album</button>
+        <button
+          style={todoButtonStyle}
+          onClick={() => onClickTabButton("todo")}
+        >
+          Todo
+        </button>
+        <button
+          style={albumButtonStyle}
+          onClick={() => onClickTabButton("album")}
+        >
+          Album
+        </button>
         <ErrorBoundary fallback={<p>Todo or AlbumListエラー</p>}>
           <Suspense fallback={<p>Todo or AlbumListローディング中</p>}>
             {selectedTab === "todo" ? <TodoList /> : <AlbumList />}
